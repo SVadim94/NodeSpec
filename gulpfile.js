0771944193cb8a0e3fb5400a46f0ed58877ae944
SVadim94 <svadim94@gmail.com>
2016-03-31 16:33:09 +0300

    Eslint was added to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 const changed = require('gulp-changed');
 
+var files = {
+    libs: 'libs/**/*.js'
+};
+
+var babelConfig = [
+    'es2015-node5',
+    'stage-3'
+];
+
 gulp.task('libs', () => {
-    gulp.src('libs/**/*.js')
+    gulp.src(files.libs)
     .pipe(changed('build'))
     .pipe(babel({
-            presets: [
-                'es2015-node5',
-                'stage-3'
-            ]
+            presets: babelConfig
      }))
      .pipe(gulp.dest('build'));
 });
@@ -19,7 +26,14 @@ gulp.task('test', () => {
 });
 
 gulp.task('watch', ['libs'], () => {
-    gulp.watch('libs/**/*.js', ['libs']);
+    gulp.watch(file.libs, ['libs']);
+});
+
+gulp.task('lint', () => {
+    gulp.src(files.libs)
+    .pipe(eslint())
+    .pipe(eslint.formatEach('compact', process.stderr))
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['watch'], () => {
